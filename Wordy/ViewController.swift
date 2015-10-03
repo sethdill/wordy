3fe7e9d8c35b31fd7823a4254df12aaf6342a231
Seth Dillingham <seth.dillingham@gmail.com>
2015-10-03 14:16:12 -0400

    Initial Commit

---
+++ Wordy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wordy
+//
+//  Created by Seth Dillingham on 10/3/2015.
+//  Copyright © 2015 Macrobyte Resources. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
